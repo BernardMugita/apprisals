ae44c36171983c8ee123af2e862ba3a2c874a34c
Marto <maritnbriston01@gmail.com>
2023-10-12 22:12:13 +0300

    added auth

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 import models
+import auth
 
 app = FastAPI()
 
@@ -13,6 +14,38 @@ async def root():
 
 @app.post("/users")
 async def getallusers(request: Request):
-    data = await request.json() # we will get this from decodeJWT
-    users = models.get_users(data["company"])
-    return users
+    auth_header = request.headers.get('Authorization')
+    if auth_header and auth_header.startswith("Bearer "):
+        auth_token = auth_header.split(" ")[1]
+        usr = auth.decodeJWT(auth_token)
+        if usr == "Invalid":
+            return "Invalid Token"
+        else:
+            users = models.get_users(usr["organization"])
+            return users
+    else:
+        return "Invalid Token" 
+
+
+@app.post("/login")
+async def login(request: Request):
+    data = await request.json()
+    email = data["email"]
+    password = data["password"]
+    company = data["company"]
+    ans = auth.login(email, password, company)
+    return ans
+
+@app.post("/createuser")
+async def createuser(request: Request):
+    data = await request.json()
+    username = data["username"]
+    roles = data["roles"]
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    email = data["email"]
+    organization = data["organization"]
+    telephone = data["telephone"]
+    job_role = data["job_role"]
+    ans = models.create_user(username, roles, first_name, last_name, email, organization, telephone, job_role)
+    return ans
--- models.py
+++ models.py
@@ -5,9 +5,11 @@ from enum import Enum as PydanticEnum
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import pymysql
+import bcrypt
 
 import uuid
 import os
+
 
 Base = declarative_base()  # Create a declarative base class for SQLAlchemy
 
@@ -62,56 +64,6 @@ class TaskStatus(str, Enum):  # Enum class for task status
     completed = "completed"
     rejected = "rejected"
 
-# Pydantic model for the user table
-
-
-# class User(Base):
-#     __tablename__ = 'users'
-
-#     id = Column(String(36), primary_key=True, default=str(uuid.uuid4()))
-#     username = Column(String(50), nullable=False)
-#     roles = Column(String(50), nullable=False)
-#     first_name = Column(String(50), nullable=False)
-#     last_name = Column(String(50), nullable=False)
-#     email = Column(String(100), nullable=False, unique=True)
-#     organization = Column(String(100), nullable=False)
-#     telephone = Column(String(20), nullable=False)
-
-
-# # Pydantic model for the employee table
-# class Tasks(Base):
-#     __tablename__ = 'tasks'
-
-#     id = Column(String(36), primary_key=True, default=str(uuid.uuid4()))
-#     title = Column(String(255), nullable=False)
-#     description = Column(String(1000), nullable=False)
-#     status = Column(String(50), nullable=False)
-#     assigned_to = Column(String(255), nullable=False) # change to foreign key??
-#     task_type = Column(String(50), nullable=False)
-#     rating = Column(Integer, nullable=False)
-#     feedback = Column(String(1000), nullable=False)
-
-# class Payslips(Base):
-#     __tablename__ = 'payslips'
-
-#     id = Column(String(36), primary_key=True, default=str(uuid.uuid4()))
-#     employee_id = Column(String(255), nullable=False) # change to foreign key??
-#     employee_name = Column(String(255), nullable=False)
-#     prepared_by = Column(String(255), nullable=False)
-#     date = Column(String(20), nullable=False)
-#     period = Column(String(20), nullable=False)
-#     amount = Column(Float, nullable=False)
-#     status = Column(String(50), nullable=False)
-
-# class Messages(Base):
-#     __tablename__ = 'messages'
-
-#     id = Column(String(36), primary_key=True, default=str(uuid.uuid4()))
-#     sender = Column(String(255), nullable=False)
-#     receiver = Column(String(255), nullable=False)
-#     date = Column(String(20), nullable=False)
-#     message = Column(String(1000), nullable=False)
-
 
 def create_users_table(company_name):
         class User(Base):
@@ -125,6 +77,8 @@ def create_users_table(company_name):
             email = Column(String(100), nullable=False, unique=True)
             organization = Column(String(100), nullable=False)
             telephone = Column(String(20), nullable=False)
+            hash = Column(String(255), nullable=False)
+            job_role = Column(String(255), nullable=False)
         return User
 
 def create_tasks_table(company_name):
@@ -192,20 +146,49 @@ def create_company_tables(company_name):
 # create_company_tables("Tesla")
 
 
-# for creating a user
-def create_user(username, roles, first_name, last_name, email, organization, telephone):
-    usr_table = create_users_table(organization) # assuming the organization is the company name
-    new_user = usr_table(username=username, roles=roles, first_name=first_name, last_name=last_name, email=email, organization=organization, telephone=telephone)
-    session.add(new_user)
-    session.commit()
-
-# use this to test the create_user function
-# create_user("DNdathi", "user", "Dennis", "Ndathi", "D.ndathi@tesla.com", "Tesla", "1234")
-
 def get_users(organization):
+    Base.metadata.clear()
     usr_table = create_users_table(organization)
     users = session.query(usr_table).all()
     return users
+
+def get_user_by_email(email, organization):
+    Base.metadata.clear() # clear the metadata and stops sqlalchemy from complaining about the table already existing
+    usr_table = create_users_table(organization)
+    if email:
+        user = session.query(usr_table).filter_by(email=email).first()
+    else:
+        user = None
+    return user
+
+
+from auth import createOTP
+# for creating a user
+def create_user(username, roles, first_name, last_name, email, organization, telephone, job_role):
+    usr_table = create_users_table(organization) # assuming the organization is the company name
+    # generate a one time password
+    otp = createOTP()
+    hash = bcrypt.hashpw(otp.encode('utf-8'), bcrypt.gensalt())
+    new_user = usr_table(username=username, roles=roles, first_name=first_name, last_name=last_name, email=email, organization=organization, telephone=telephone, hash=hash, job_role=job_role)
+    session.add(new_user)
+    session.commit()
+    usr_obj = session.query(usr_table).filter_by(email=email).filter_by(first_name=first_name).first()
+    res = {
+        "id": usr_obj.id,
+        "username": usr_obj.username,
+        "roles": usr_obj.roles,
+        "first_name": usr_obj.first_name,
+        "last_name": usr_obj.last_name,
+        "email": usr_obj.email,
+        "organization": usr_obj.organization,
+        "telephone": usr_obj.telephone,
+        "job_role": usr_obj.job_role,
+        "OTP": otp
+    }
+    return res
+
+# use this to test the create_user function
+# print(create_user("AMionwa", "admin", "Andy", "Mionwa", "andymm@tesla.com", "Tesla", "669875421", "Lead Engineer"))
 
 # use this to test the get_users function
 # for user in get_users("Tesla"):
